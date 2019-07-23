569e7367b583db48b1ff26329c508b7dbb2a4e49
OMIsie <omisie@gmail.com>
2019-07-23 14:58:49 +0200

    Initial commit

---
+++ app/src/main/java/io/github/omisie11/kalkulatordawek/Leki.kt
@@ -0,0 +1,31 @@
+package io.github.omisie11.kalkulatordawek
+
+open class Lek {
+    open val name = "Lek"
+    open val singleMin: Double = 0.0
+    open val singleMax: Double = 0.0
+    open val dailyMin: Double = 0.0
+    open val dailyMax: Double = 0.0
+    open val count: Int = 0
+    open val max: Int = 0
+}
+
+class Ibuprofen : Lek() {
+    override val name = "Ibuprofen"
+    override val singleMin: Double = 20.0 / 3
+    override val singleMax: Double = 30.0 / 3
+    override val dailyMin: Double = 20.0
+    override val dailyMax: Double = 30.0
+    override val count: Int = 3
+    override val max: Int = 1200
+}
+
+class Paracetamol : Lek() {
+    override val name = "Paracetamol"
+    override val singleMin: Double = 15.0
+    override val singleMax: Double = 15.0
+    override val dailyMin: Double = 60.0
+    override val dailyMax: Double = 60.0
+    override val count: Int = 4
+    override val max: Int = 4000
+}
---
+++ app/src/main/java/io/github/omisie11/kalkulatordawek/MainActivity.kt
@@ -0,0 +1,107 @@
+package io.github.omisie11.kalkulatordawek
+
+import android.os.Bundle
+import android.view.View
+import android.widget.RadioButton
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        radio_ibuprofen.isChecked = true
+
+        button_licz.setOnClickListener {
+            val lek = if (findViewById<RadioButton>(radio_group_lek.checkedRadioButtonId) == radio_ibuprofen)
+                Ibuprofen() else Paracetamol()
+
+            text_wynik.text = wyliczDawke(lek, edit_text_substancja.text.toString().toDouble(),
+                edit_text_syrop.text.toString().toDouble(), edit_text_masa.text.toString().toDouble())
+        }
+    }
+
+    fun wyliczDawke(lek: Lek, stezenieSubstancji: Double, iloscSyropu: Double, masaCiala: Double): String {
+        if (stezenieSubstancji == 0.0 || iloscSyropu == 0.0 || masaCiala == 0.0) return "Błędne wartości"
+
+        var informacja: String = ""
+
+        val stezenie = stezenieSubstancji / iloscSyropu
+
+        var lekMinMg: Double = lek.singleMin * masaCiala
+        var lekDailyMinMg: Double = lek.dailyMin * masaCiala
+        var lekMaxMg: Double = lek.singleMax * masaCiala
+        var lekDailyMaxMg: Double = lek.dailyMax * masaCiala
+
+        var uwaga = ""
+
+        if (lekDailyMaxMg > lek.max) {
+            lekDailyMinMg = lek.max.toDouble()
+            lekDailyMaxMg = lek.max.toDouble()
+            lekMinMg = (lek.max / lek.count).toDouble()
+            lekMaxMg = (lek.max / lek.count).toDouble()
+
+            uwaga = "Pamiętaj, że maksymalna dopuszczalna dawka dobowa ${lek.name}u dla osoby dorosłej " +
+                    "wynosi $lekDailyMaxMg mg."
+
+            if (lek is Ibuprofen) uwaga += "\n Większe dawki leku można przyjmować jedynie pod nadzorem i na " +
+                    "zlecenie lekarza."
+        }
+
+        informacja += uwaga
+
+        //lekMinMg = Math.round(lekMinMg * 100) / 100
+        //lekDailyMinMg = Math.round(lek_daily_min_ml * 100) / 100
+        //lekMaxMg = Math.round(lek_max_ml * 100) / 100
+        //lekDailyMaxMg = Math.round(lek_daily_max_ml * 100) / 100
+
+        var lekMinMl: Double = lekMinMg / stezenie
+        var lekDailyMinMl: Double = lekDailyMinMg / stezenie
+        var lekMaxMl: Double = lekMaxMg / stezenie
+        var lekDailyMaxMl: Double = lekDailyMaxMg / stezenie
+
+        //lek_min_ml = Math.round(lek_min_ml * 100) / 100
+        //lek_daily_min_ml = Math.round(lek_daily_min_ml * 100) / 100
+        //lek_max_ml = Math.round(lek_max_ml * 100) / 100
+        //lek_daily_max_ml = Math.round(lek_daily_max_ml * 100) / 100
+
+        var tekst = if (lekDailyMinMl != lekDailyMaxMl) {
+            "Jednorazowa dawka: $lekMinMl-$lekMaxMl ml (odpowiednik $lekMinMg-$lekMaxMg mg " +
+                    "${lek.name}u) " +
+                    "\nMożna podać ${lek.count} takie dawki w ciągu doby. " +
+                    "\nDobowa dawka: $lekDailyMinMl-$lekDailyMaxMl ml (odpowiednik $lekDailyMinMg-$lekDailyMaxMg " +
+                    "mg ${lek.name}u)"
+        } else {
+            "Jednorazowa dawka: $lekMinMl ml (odpowiednik $lekMinMg mg ${lek.name}u) " +
+                    "\nMożna podać ${lek.count} takie dawki w ciągu doby. " +
+                    "\nDobowa dawka: $lekDailyMinMl ml (odpowiednik $lekDailyMinMg mg" +
+                    " ${lek.name}u)"
+        }
+
+        informacja += tekst
+
+        return informacja
+    }
+
+    fun onRadioButtonClicked(view: View) {
+        if (view is RadioButton) {
+            // Is the button now checked?
+            val checked = view.isChecked
+
+            // Check which radio button was clicked
+            when (view.getId()) {
+                R.id.radio_ibuprofen ->
+                    if (checked) {
+                        Toast.makeText(this, "Ibuprofen", Toast.LENGTH_SHORT).show()
+                    }
+                R.id.radio_paracetamol ->
+                    if (checked) {
+                        Toast.makeText(this, "Paracetamol", Toast.LENGTH_SHORT).show()
+                    }
+            }
+        }
+    }
+}
